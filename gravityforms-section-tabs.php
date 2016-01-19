a297063b35a19bea9b2411fce7ab97c207d7b65f
Marin Atanasov <tyxla@abv.bg>
2016-01-19 21:22:25 +0200

    make GFSectionTabsAddon::gform_pre_render()  optional, fixes #15

--- gravityforms-section-tabs.php
+++ gravityforms-section-tabs.php
@@ -108,7 +108,7 @@ if (class_exists("GFForms")) {
 		}
 
 		// add a specific CSS class to forms with section tabs enabled
-		public function gform_pre_render($form, $ajax, $field_values) {
+		public function gform_pre_render($form, $ajax, $field_values = array()) {
 			if (!empty($form['gravityforms-section-tabs']['enable_section_tabs'])) {
 				$classname = 'gravity_forms_section_tabs_enabled';
 				if (empty($form['cssClass'])) {
